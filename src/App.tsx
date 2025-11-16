1fa1627a605fccc15ce15a32b067d2fbb5b0038c
Kenneth C. Arnold <kcarnold@alum.mit.edu>
2025-11-15 21:26:44 -0500

    note-taker direct link

--- src/App.tsx
+++ src/App.tsx
@@ -252,7 +252,8 @@ function HomePage() {
             </div>
           );
         })}
-        <a className="underline text-blue-600 hover:text-blue-800 dark:text-blue-400 dark:hover:text-blue-300" href="/transcript,sourceText|translatedOutline-French,video#editor">Transcriber</a>
+        <a className="underline text-blue-600 hover:text-blue-800 dark:text-blue-400 dark:hover:text-blue-300" href="/transcript,sourceText|translatedOutline-French,video#editor">Transcriber</a> |
+        <a className="underline text-blue-600 hover:text-blue-800 dark:text-blue-400 dark:hover:text-blue-300" href="/sourceText|translatedOutline-French#editor">Note-Taker</a>
       </div>
     </div>
   );
