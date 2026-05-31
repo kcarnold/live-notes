f731e15ff3ca5ebdf7358780442e85d743c4fa1a
Kenneth C. Arnold <kcarnold@alum.mit.edu>
2026-05-30 20:32:11 -0400

    remove old Markdown translation pipeline

--- src/translationUtils.ts
+++ src/translationUtils.ts
@@ -21,72 +21,12 @@ export function findContiguousBlocks(arr: any[]) {
     return blocks;
 }
 
-export interface DecomposedChunk {
-    format: string;
-    content: string;
-    trailingWhitespace: string;
-};
-
-export function decompose(chunk: string): DecomposedChunk {
-    const content = chunk.trimEnd();
-    const trailingWhitespace = chunk.slice(content.length);
-    // formatting content is: leading whitespace, -, *, or # / ## / ...
-    // content is: everything else
-    const splits = /^([\s\d\-\*\#\.]*)(.*)$/.exec(content);
-    if (splits) {
-        return { format: splits[1], content: splits[2], trailingWhitespace };
-    } else {
-        console.warn('Failed to decompose chunk:', chunk);
-        return { format: '', content: content, trailingWhitespace };
-    }
-}
-
 
 export interface TranslationTodo {
     chunks: string[]; // the "content" part of the chunk; the formatting part is added back later
     offset: number;
     isTranslationNeeded: boolean[];
     translatedContext: string;
-}
-
-
-export function getDecomposedChunks(text: string) {
-    // Split the input into chunks (for first pass, just do by line)
-    let chunks = text.split('\n');
-
-    // Whitespace is annoying, so consolidate any whitespace-only chunk into the previous chunk.
-    chunks = chunks.reduce((acc: string[], chunk: string) => {
-        if (acc.length === 0) {
-            // First chunk, just add it
-            acc.push(chunk);
-            return acc;
-        }
-        if (chunk.trim() === '') {
-            acc[acc.length - 1] += '\n' + chunk;
-        }
-        else {
-            // It's possible that the first chunk was whitespace-only.
-            // In that case, we need to add it to the previous chunk.
-            if (acc[acc.length - 1].trim() === '') {
-                acc[acc.length - 1] += '\n' + chunk;
-            } else {
-                // Otherwise, just add it as a new chunk
-                acc.push(chunk);
-            }
-        }
-        return acc;
-    }, []);
-
-    // Assert that all chunks are non-empty
-    for (const chunk of chunks) {
-        if (chunk.trim() === '') {
-            console.error('Empty chunk found:', chunk);
-        }
-    }
-
-    // Decompose chunks into formatting and content sections
-    const decomposedChunks = chunks.map(decompose);
-    return decomposedChunks;
 }
 
 // We need a type that is a map of string to string. The translation cache is actually a YMap, which has a slightly different
@@ -109,59 +49,6 @@ export function translationCacheKey(language: string, chunkText: string) {
     // The translation cache key is a combination of the language and the chunk text.
     // This is to avoid collisions between different languages.
     return `${language}:${chunkText}`;
-}
-
-export function getTranslationTodos(language: string, decomposedChunks: DecomposedChunk[], translationCache: TranslationCache) {
-    // Make an array where each entry is:
-    // 0: don't include
-    // 1: need to translate
-    // 2: included only for context
-    // The keys of the translation cache are always the trimmed chunks.
-    const chunkStatus = decomposedChunks.map((chunk) => {
-        return (chunk.content === "" || translationCache.has(translationCacheKey(language, chunk.content))) ? 0 : 1;
-    }) as ChunkStatus[];
-
-    // Mark a few lines before each "need to translate" chunk as "context"
-    for (let i = 0; i < chunkStatus.length; i++) {
-        if (chunkStatus[i] === 1) {
-            // Mark the previous few lines as context
-            for (let j = 1; j <= 3; j++) {
-                if (i - j >= 0 && chunkStatus[i - j] === 0) {
-                    chunkStatus[i - j] = 2;
-                }
-            }
-        }
-    }
-
-    // Create contiguous blocks of text to translate
-    const translationTodoBlocks = findContiguousBlocks(chunkStatus);
-
-    // Now make a to-do list for all translations to request. Translations will get requested in blocks, where
-    // each block is a contiguous range of chunks that need to be translated.
-
-    const translationTodos: TranslationTodo[] = [];
-    for (const block of translationTodoBlocks) {
-        const [start, end] = block;
-        const chunksInContext = decomposedChunks.slice(start, end + 1);
-        const statusesInContext = chunkStatus.slice(start, end + 1);
-        const translatedContext = chunksInContext.map((chunk) => {
-            const key = translationCacheKey(language, chunk.content);
-            const cachedTranslation = translationCache.get(key);
-            if (cachedTranslation) {
-                return chunk.format + cachedTranslation + chunk.trailingWhitespace;
-            }
-            return '';
-        }).join('\n');
-        const isTranslationNeeded = statusesInContext.map(x => x === 1);
-        translationTodos.push({
-            chunks: chunksInContext.map((chunk) => chunk.content),
-            offset: start,
-            isTranslationNeeded,
-            translatedContext,
-        });
-    }
-    return translationTodos;
-
 }
 
 export function updateTranslationCache(serverResponse: any, translationCache: TranslationCache) {
