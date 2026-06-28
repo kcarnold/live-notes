0aa679f87c79ae51aaf7d6ecaa2f62b71351f45b
Kenneth C. Arnold <kcarnold@alum.mit.edu>
2026-06-28 09:55:38 -0400

    fix deploy git sha

--- deploy-docker-compose.sh
+++ deploy-docker-compose.sh
@@ -2,5 +2,6 @@
 set -e
 
 git pull
-GIT_SHA=$(git rev-parse HEAD) docker compose -f compose.yaml -f compose.prod.yaml build
+export GIT_SHA=$(git rev-parse HEAD)
+docker compose -f compose.yaml -f compose.prod.yaml build
 docker compose -f compose.yaml -f compose.prod.yaml up -d
