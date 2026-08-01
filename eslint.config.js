00945d9686cfc834cb70ff331bf1402c521c47f5
Kenneth C. Arnold <kcarnold@alum.mit.edu>
2026-08-01 19:56:53 -0400

    ignore build files in the audio feeder

--- eslint.config.js
+++ eslint.config.js
@@ -7,7 +7,7 @@ import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
 
 export default tseslint.config(
-  { ignores: ['dist'] },
+  { ignores: ['dist', 'macos-audio-feeder/.build'] },
   {
     extends: [
       js.configs.recommended,
